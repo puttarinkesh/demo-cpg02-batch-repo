6c2ff0177d721fdbd639bfca3163145ba1cff515
Rinkesh <rinkesh.putta@xoriantcorp.onmicrosoft.com>
2025-11-11 08:53:43 +0530

    added the data types of terraform

--- providers.tf
+++ providers.tf
@@ -1,15 +1,38 @@
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = "4.52.0"
+#     }
+#   }
+# }
+
+# provider "azurerm" {
+#   # Configuration options
+#   features {
+#   }
+#   subscription_id = "0f87b7f3-b8ff-4c25-b09a-901f6869202e"
+# }
+
+
+
+
+
+
+
 terraform {
+  required_version = ">1.12.2" #This is Terraform CLI version - https://developer.hashicorp.com/terraform/install
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.52.0"
+      version = ">=4.50.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
-  features {
-  }
+  features {}
   subscription_id = "0f87b7f3-b8ff-4c25-b09a-901f6869202e"
 }
+
+# AzureRM version
--- resource_group.tf
+++ resource_group.tf
@@ -51,21 +51,33 @@
 # az account set --subscription xxx-xxxx-xxx-xxx
 # az account show
 
+# resource "azurerm_resource_group" "demo_cpg02-03" {
+#     name = "rg-hdfc-app_ravindra"
+#     location = "east us"
+# }
 
-
-resource "azurerm_resource_group" "demo-cpg02-01" {
-    name = "rg-demo-cpg02-01"
-    location = "central india"
+/*  start of the comment
+#Virtual Network
+resource "azurerm_virtual_network" "vnet_01" {
+  name = "vnet-icici-cpg02-01"
+  resource_group_name = azurerm_resource_group.demo_cpg02.name
+  location = azurerm_resource_group.demo_cpg02.location
+  address_space = ["192.168.0.0/24"]
+  dns_servers = ["10.10.0.0"]
+  private_endpoint_vnet_policies = "Disabled"
 }
 
-resource "azurerm_resource_group" "demo_cpg02-02" {
-    name = "rg-hdfc-app-01"
-    location = "east us"
-}
-
+resource "azurerm_virtual_network" "vnet_02" {
+  name = "vnet-hdfc-cpg02-01"
+  resource_group_name = azurerm_resource_group.demo_cpg02.name
+  location = azurerm_resource_group.demo_cpg02.location
+  address_space = ["10.0.0.0/24"]
+  dns_servers = []
+  private_endpoint_vnet_policies = "Disabled"
+}  end of the comment
+*/ 
 
 # Storage Account
-
 resource "azurerm_storage_account" "demo_stor_acc" {
     name = "democpg02storageaccount"
     resource_group_name = "rg-demo-cpg02-01"
@@ -75,20 +87,55 @@ resource "azurerm_storage_account" "demo_stor_acc" {
     account_kind = "FileStorage"
 }
 
+resource "azurerm_storage_account" "demo_stor_acc_23" {
+    name = "democpg73652389hfk"
+    resource_group_name = "rg-demo-cpg02-01"
+    location = "central india"
+    account_tier = "Premium"
+    account_replication_type = "LRS"
+}
+
+# #1st resource
+# resource "azurerm_resource_group" "demo-cpg02-01" {
+#     name = "rg-demo-cpg02-01"
+#     location = "south india"
+# }
+
+#4th resource
+# This resource should be created after rg and storage account creation due to Application dependency
+resource "azurerm_resource_group" "demo_cpg02-02" {
+    name = "rg-hdfc-app-01"
+    location = "east us"
+
+    depends_on = [azurerm_storage_account.demo_stor_83w67w7acc, azurerm_resource_group.demo_cpg02]
+}
 
 
+#2nd resource
+resource "azurerm_storage_account" "demo_stor_83w67w7acc" {
+    name = "democpg8777q23"
+    resource_group_name = azurerm_resource_group.demo-cpg02-01.name
+    location = azurerm_resource_group.demo-cpg02-01.location
+    account_tier = "Premium"
+    account_replication_type = "LRS"
+}
 
-# resource "azurerm_storage_account" "demo_stor_acc_01" {
-#     name = "democpg-storage_account"
-#     resource_group_name = "rg-demo-cpg02-01"
-#     location = "central india"
-#     account_tier = "Premium"
-#     account_replication_type = "LRS"
+# #1std resource
+# resource "azurerm_resource_group" "demo_cpg02" {
+#     name = "rg-icici-app-01"
+#     location = "west europe"
 # }
 
 
+#Dependencies
+# Implicit - resource referencing 
+# Explicit - Explicitly add the depency without resource referencing
 
-resource "azurerm_resource_group" "demo_cpg02-03" {
-    name = "rg-hdfc-app_ravindra"
-    location = "east us"
-}
+#comments #ignore
+# single line comments    - # or //
+# multi line comments     - /* */
+
+
+
+
+
