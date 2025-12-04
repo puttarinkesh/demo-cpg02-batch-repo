5e429c4f4b03f06aa41e9d5d13301935fa7b50cd
Rinkesh <rinkesh.putta@xoriantcorp.onmicrosoft.com>
2025-12-04 08:42:13 +0530

    added the terraform module code

--- mcd_client/main.tf
+++ mcd_client/main.tf
@@ -1,16 +1,6 @@
 
 
-# #Creating VM Using Module Code
-# module "rinkesh_vm" {                #syntax   # user defined 
-#     source = "../modules/windows_virtual_machine"
-#     env = ""
-#     postfix = ""
-#     app = ""
-#     location = ""
-# }   
-
-
-
+# #Creating Resource Group Module Code
 module "dev_rg" {
   source   = "../modules/resource_group"
   env      = "md-rafi"
@@ -25,7 +15,34 @@ module "dev_rg" {
 }
 
 
-#C:\Users\Rinkesh\demo-cpg02-batch-repo\modules\windows_virtual_machine
-#C:\Users\Rinkesh\demo-cpg02-batch-repo\mcd_client
-# modules\windows_virtual_machine
+#Creating module for Virtual Machine
+module "user_defined_vm" {
+  source                         = "../modules/windows_virtual_machine"
+  env                            = "prod"
+  postfix                        = "inventory"
+  app                            = "McD"
+  location                       = "east us"
+  subnet_name                    = "vm-subnet"
+  virtual_network_name           = "aks-vnet-41970527"
+  virtual_network_rg             = "MC_god-father-aks-app-demo_god-father-aks-app-cluster_eastus"
+  private_ip_address_allocation  = "Dynamic" #3"Static"
+  module_vm_name                 = "001"
+  module_vm_size                 = "Standard_B8s_v2" #"Standard_D2s_v2"
+  module_vm_admin_username       = "mcdadmin"
+  module_vm_admin_password       = "HGolnhTCTYV54$$@"
+  module_vm_storage_account_type = "Premium_LRS"
+  tags = {
+    "architect"   = "shabnam"
+    "developer"   = "sai ram"
+    "project_man" = "murali"
+  }
+}
+
+
+
+
+
+
+
+
 
