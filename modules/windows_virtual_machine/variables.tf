ef52fc4053d1b623d477ba03af590b798fa1347e
Rinkesh <rinkesh.putta@xoriantcorp.onmicrosoft.com>
2025-12-05 07:42:07 +0530

    added the terraform module code

--- modules/windows_virtual_machine/variables.tf
+++ modules/windows_virtual_machine/variables.tf
@@ -15,74 +15,74 @@ variable "location" {
   default = null
 }
 
-# #NIC Variables 
-# variable "nic_name" {
-#   description = "(Required) The name of the Network Interface. Changing this forces a new resource to be created."
-#   type        = string
-#   default     = null
-# }
-# variable "nic_name_ip_configuration" {
+#NIC Variables 
+variable "nic_name" {
+  description = "(Required) The name of the Network Interface. Changing this forces a new resource to be created."
+  type        = string
+  default     = null
+}
+variable "nic_name_ip_configuration" {
+  description = ""
+  type        = string
+  default     = null
+}
+variable "module_vm_admin_username" {
+  description = "(Optional) The ID of the Subnet where this Network Interface should be located in."
+  type        = string
+  default     = null
+}
+variable "module_vm_storage_account_type" {
+  description = "(Optional) The Type of Storage Account which should back this the Internal OS Disk. Possible values are Standard_LRS, StandardSSD_LRS, Premium_LRS, StandardSSD_ZRS and Premium_ZRS. Changing this forces a new resource to be created."
+  type        = string
+  default     = "Standard_LRS"
+}
+variable "module_vm_name" {
+  description = "(Required) The name of the Windows Virtual Machine. Changing this forces a new resource to be created."
+  type        = string
+  default     = null
+}
+variable "module_vm_admin_password" {
+  description = ""
+  type        = string
+  default     = null
+}
+variable "module_vm_size" {
+  description = "vm size"
+  type        = string
+  default     = "Standard_DS2_v2" #"Standard_D16a_v4"  #"Standard_B2as_v2"  #
+}
+variable "module_vm_caching" {
+  description = "vm disk caching"
+  type        = string
+  default     = "ReadWrite"
+}
+variable "tags" {}
+variable "rg_name" {
+  description = "rg name for straoge account"
+  type        = string
+  default     = null
+}
+variable "private_ip_address_allocation" {
+  description = "private_ip_address_allocation"
+  type        = string
+  default     = "Dynamic"
+}
+# variable "" {
 #   description = ""
-#   type        = string
-#   default     = null
+#   type = string
+#   default = ""
 # }
-# variable "module_vm_admin_username" {
-#   description = "(Optional) The ID of the Subnet where this Network Interface should be located in."
-#   type        = string
-#   default     = null
-# }
-# variable "module_vm_storage_account_type" {
-#   description = "(Optional) The Type of Storage Account which should back this the Internal OS Disk. Possible values are Standard_LRS, StandardSSD_LRS, Premium_LRS, StandardSSD_ZRS and Premium_ZRS. Changing this forces a new resource to be created."
-#   type        = string
-#   default     = "Standard_LRS"
-# }
-# variable "module_vm_name" {
-#   description = "(Required) The name of the Windows Virtual Machine. Changing this forces a new resource to be created."
-#   type        = string
-#   default     = null
-# }
-# variable "module_vm_admin_password" {
-#   description = ""
-#   type        = string
-#   default     = null
-# }
-# variable "module_vm_size" {
-#   description = "vm size"
-#   type        = string
-#   default     = "Standard_DS2_v2" #"Standard_D16a_v4"  #"Standard_B2as_v2"  #
-# }
-# variable "module_vm_caching" {
-#   description = "vm disk caching"
-#   type        = string
-#   default     = "ReadWrite"
-# }
-# variable "tags" {}
-# variable "rg_name" {
-#   description = "rg name for straoge account"
-#   type        = string
-#   default     = null
-# }
-# variable "private_ip_address_allocation" {
-#   description = "private_ip_address_allocation"
-#   type        = string
-#   default     = "Dynamic"
-# }
-# # variable "" {
-# #   description = ""
-# #   type = string
-# #   default = ""
-# # }
 
-# #Subnet Variables
-# variable "subnet_name" {
-#   description = "subnet name should be passed"
-#   type        = string
-# }
-# variable "virtual_network_name" {
-#   description = "virtual_network_name"
-#   type        = string
-# }
-# variable "virtual_network_rg" {
-#   description = "virtual_network_rg"
-#   type        = string
-# }
+#Subnet Variables
+variable "subnet_name" {
+  description = "subnet name should be passed"
+  type        = string
+}
+variable "virtual_network_name" {
+  description = "virtual_network_name"
+  type        = string
+}
+variable "virtual_network_rg" {
+  description = "virtual_network_rg"
+  type        = string
+}
